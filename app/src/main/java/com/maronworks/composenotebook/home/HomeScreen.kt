0c2249bbb50f774d59515142d5db9ea475fe0dde
ralphmaron <ralphmaron.dev@gmail.com>
2024-03-28 12:44:13 +0800

    added feature-card

--- app/src/main/java/com/maronworks/composenotebook/home/HomeScreen.kt
+++ app/src/main/java/com/maronworks/composenotebook/home/HomeScreen.kt
@@ -1,19 +1,22 @@
 package com.maronworks.composenotebook.home
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.maronworks.composenotebook.R
+import com.maronworks.composenotebook.home.components.FeatureCard
+import com.maronworks.composenotebook.ui.theme.ComposeNotebookTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -33,14 +36,28 @@ fun HomeScreen() {
             )
         }
     ) { innerPadding ->
-        Box(
+        LazyVerticalGrid(
             modifier = Modifier
-                .fillMaxSize()
                 .padding(innerPadding),
-            contentAlignment = Alignment.Center
+            columns = GridCells.Fixed(2)
         ) {
-            Text(text = "Home Screen")
+            items(2) {
+                FeatureCard(
+                    modifier = Modifier
+                        .padding(10.dp),
+                    image = R.drawable.sample_image,
+                    title = "Programming",
+                    subTitle = "is fun"
+                )
+            }
         }
     }
+}
 
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun Default() {
+    ComposeNotebookTheme {
+        HomeScreen()
+    }
 }
