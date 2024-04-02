906762d7500d8f759cb0859f0b6e3a1b4f94ba8a
ralphmaron <ralphmaron.dev@gmail.com>
2024-04-02 22:58:45 +0800

    fixed: navigating from [todo-home-screen] to [new-note-screen]

--- app/src/main/java/com/maronworks/composenotebook/features/todo_app/TodoAppViewModel.kt
+++ app/src/main/java/com/maronworks/composenotebook/features/todo_app/TodoAppViewModel.kt
@@ -1,13 +1,27 @@
 package com.maronworks.composenotebook.features.todo_app
 
 import android.util.Log
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 class TodoAppViewModel : ViewModel() {
     var newNote = mutableStateOf("")
+    private var selectedScreen = mutableIntStateOf(0)
 
     fun saveNote() {
         Log.d("todo_app", "Saving note...\nContents: ${newNote.value}")
+    }
+
+    fun getSelectedScreen(): Int{
+        return selectedScreen.intValue
+    }
+
+    fun gotoNewNoteScreen() {
+        selectedScreen.intValue = 1
+    }
+
+    fun resetSelectedScreen() {
+        selectedScreen.intValue = 0
     }
 }
