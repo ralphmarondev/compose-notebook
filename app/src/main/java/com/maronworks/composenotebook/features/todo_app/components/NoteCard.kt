37f03bb7bae218aa0b38a29c3d86d89c6dbee2b1
ralphmaron <ralphmaron.dev@gmail.com>
2024-04-02 22:24:27 +0800

    added todo-app

---
+++ app/src/main/java/com/maronworks/composenotebook/features/todo_app/components/NoteCard.kt
@@ -0,0 +1,37 @@
+package com.maronworks.composenotebook.features.todo_app.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NoteCard(
+    modifier: Modifier = Modifier
+) {
+    ElevatedCard(
+        modifier = modifier,
+        colors = CardDefaults.elevatedCardColors(
+            containerColor = MaterialTheme.colorScheme.tertiary,
+            contentColor = MaterialTheme.colorScheme.onTertiary
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .defaultMinSize(minHeight = 50.dp)
+                .padding(10.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Some notes lol")
+        }
+    }
+}
