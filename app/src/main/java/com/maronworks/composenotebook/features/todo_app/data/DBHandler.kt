52de9f7c0f70d4a0e2d5996fe679fd77631d3d97
ralphmaron <ralphmaron.dev@gmail.com>
2024-04-03 03:15:52 +0800

    added DBHandler

---
+++ app/src/main/java/com/maronworks/composenotebook/features/todo_app/data/DBHandler.kt
@@ -0,0 +1,48 @@
+package com.maronworks.composenotebook.features.todo_app.data
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBHandler(context: Context) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
+    companion object {
+        private const val DB_NAME = "app_db"
+        private const val DB_VERSION = 1
+
+        const val NOTES_TABLE = "notes_table"
+        private const val NOTES_ID = "notes_id"
+        private const val NOTES_TITLE = "notes_title"
+        private const val NOTES_CONTENT = "notes_content"
+        private const val DATE_ADDED = "date_added"
+        private const val CREATE_NOTES_TABLE_QUERY =
+            "CREATE TABLE $NOTES_TABLE (" +
+                    "$NOTES_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "$NOTES_TITLE TEXT NOT NULL, " +
+                    "$NOTES_CONTENT TEXT NOT NULL, " +
+                    "$DATE_ADDED TEXT NOT NULL)"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(CREATE_NOTES_TABLE_QUERY)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS $NOTES_TABLE")
+    }
+
+    fun addNotes() {
+        // TODO: Implement this
+    }
+
+    fun readNotes() {
+        // TODO: Implement this
+    }
+
+    fun updateNotes() {
+        // TODO: Implement this
+    }
+
+    fun deleteNotes() {
+        // TODO: Implement this
+    }
+}
