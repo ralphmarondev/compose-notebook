839bd877af9ca2f284a9790f7820bbf9797181bc
ralphmaron <ralphmaron.dev@gmail.com>
2024-04-26 20:40:39 +0800

    added shared-preferences-manager for onboarding-activity

--- app/src/main/java/com/maronworks/composenotebook/core/navigation/AppNavigation.kt
+++ app/src/main/java/com/maronworks/composenotebook/core/navigation/AppNavigation.kt
@@ -1,24 +1,35 @@
 package com.maronworks.composenotebook.core.navigation
 
+import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.maronworks.composenotebook.authentication.LoginSignUpActivity
+import com.maronworks.composenotebook.core.data.local.preferences.SharedPreferenceManager
 import com.maronworks.composenotebook.home.HomeActivity
 import com.maronworks.composenotebook.onboarding.OnBoardingActivity
 
 @Composable
 fun AppNavigation(
+    context: Context = LocalContext.current,
     navController: NavHostController,
 ) {
+    val sharedPreference = SharedPreferenceManager(context)
+    val startDestination =
+        if (sharedPreference.isFirstLaunch()) Screen.OnBoarding.route else Screen.Login.route
+
     NavHost(
         navController = navController,
-        startDestination = Screen.OnBoarding.route
+        startDestination = startDestination
     ) {
         composable(Screen.OnBoarding.route) {
             OnBoardingActivity(
                 onGetStarted = {
+                    // save to preference [on-boarding complete]
+                    sharedPreference.setOnBoardingCompleted()
+
                     navController.popBackStack()
                     navController.navigate(Screen.Login.route)
                 }
--- app/src/main/java/com/maronworks/composenotebook/onboarding/components/TitleDescription.kt
+++ app/src/main/java/com/maronworks/composenotebook/onboarding/components/TitleDescription.kt
@@ -1,10 +1,13 @@
 package com.maronworks.composenotebook.onboarding.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -21,6 +24,9 @@ fun TitleDescription(
             fontSize = 20.sp,
             fontWeight = FontWeight.W500
         )
+
+        Spacer(modifier = Modifier.height(5.dp))
+
         Text(
             text = description,
             minLines = 2
