ad18c4561cf5a51a55e3899fedc6559d55b722ff
ralphmaron <ralphmaron.dev@gmail.com>
2024-04-30 00:03:50 +0800

    updated [instagram] image card

--- app/src/main/java/com/maronworks/composenotebook/mini_apps/instagram/home/components/ImageCard.kt
+++ app/src/main/java/com/maronworks/composenotebook/mini_apps/instagram/home/components/ImageCard.kt
@@ -19,12 +19,15 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.Send
 import androidx.compose.material.icons.outlined.AddComment
 import androidx.compose.material.icons.outlined.BookmarkAdd
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material.icons.outlined.MusicNote
+import androidx.compose.material.icons.outlined.MusicOff
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
@@ -33,6 +36,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -40,8 +47,10 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.maronworks.composenotebook.R
 import com.maronworks.composenotebook.mini_apps.instagram.home.domain.post.PostModel
 import com.maronworks.composenotebook.mini_apps.instagram.ui.theme.InstagramTheme
@@ -55,6 +64,9 @@ fun ImageCard(
 ) {
     val pagerState = rememberPagerState {
         post.images.size
+    }
+    var isMusicPlaying by rememberSaveable {
+        mutableStateOf(false)
     }
 
     ElevatedCard(
@@ -105,6 +117,7 @@ fun ImageCard(
                     .fillMaxWidth()
                     .wrapContentHeight()
             ) {
+                // list of images that can be swippeable
                 HorizontalPager(
                     state = pagerState,
                     modifier = Modifier
@@ -120,23 +133,73 @@ fun ImageCard(
                     )
                 }
 
-                Row(
-                    modifier = Modifier
-                        .align(Alignment.BottomCenter)
-                        .padding(15.dp)
-                ) {
-                    repeat(pagerState.pageCount) { index ->
-                        val color =
-                            if (pagerState.currentPage == index) MaterialTheme.colorScheme.primary else Color.LightGray
+                // only show pager and page indicator when there are 2 or more images
+                if (pagerState.pageCount > 1) {
+                    // pager
+                    Row(
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .padding(15.dp)
+                    ) {
+                        repeat(pagerState.pageCount) { index ->
+                            val color =
+                                if (pagerState.currentPage == index) MaterialTheme.colorScheme.primary else Color.LightGray
 
-                        Spacer(modifier = Modifier.width(2.dp))
-                        Box(
+                            Spacer(modifier = Modifier.width(2.dp))
+                            Box(
+                                modifier = Modifier
+                                    .size(12.dp)
+                                    .clip(CircleShape)
+                                    .background(color)
+                            )
+                            Spacer(modifier = Modifier.width(2.dp))
+                        }
+                    }
+
+                    // page indicator
+                    Box(
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .padding(15.dp)
+                            .clip(RoundedCornerShape(10.dp))
+                            .background(MaterialTheme.colorScheme.tertiary)
+                            .align(Alignment.TopEnd)
+                    ) {
+                        Text(
+                            text = "${pagerState.currentPage + 1}/${pagerState.pageCount}",
+                            fontWeight = FontWeight.W300,
+                            fontSize = 14.sp,
+                            color = MaterialTheme.colorScheme.onTertiary,
                             modifier = Modifier
-                                .size(12.dp)
-                                .clip(CircleShape)
-                                .background(color)
+                                .padding(horizontal = 5.dp, vertical = 2.dp)
                         )
-                        Spacer(modifier = Modifier.width(2.dp))
+                    }
+                }
+
+                // button to play music
+                Box(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(15.dp)
+                        .align(Alignment.BottomEnd)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.tertiary)
+                ) {
+                    IconButton(
+                        onClick = {
+                            isMusicPlaying = !isMusicPlaying
+                        },
+                        modifier = Modifier
+                            .size(28.dp)
+                            .clip(CircleShape)
+                    ) {
+                        Icon(
+                            imageVector = if (isMusicPlaying) Icons.Outlined.MusicNote else Icons.Outlined.MusicOff,
+                            contentDescription = "",
+                            tint = MaterialTheme.colorScheme.onPrimary,
+                            modifier = Modifier
+                                .size(16.dp)
+                        )
                     }
                 }
             }
